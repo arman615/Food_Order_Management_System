8647034bcd0f31b2faf6f85ba793931397bfd613
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 23:35:01 +0400

    Create MenuItem.cpp

---
+++ MenuItem.cpp
@@ -0,0 +1,12 @@
+#include "MenuItem.h"
+
+MenuItem::MenuItem(const std::string& name, double price)
+    : name(name), price(price) {}
+
+const std::string& MenuItem::getName() const {
+    return name;
+}
+
+double MenuItem::getPrice() const {
+    return price;
+}
